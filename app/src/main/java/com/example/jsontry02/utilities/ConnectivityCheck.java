21a76bb9af6eb9756815e0bb2fe0889416125adc
RishiKetuChand <rishiketuchand64@gmail.com>
2021-06-29 22:16:46 +0530

    Add Internet Check feature(universal files)

---
+++ app/src/main/java/com/example/jsontry02/utilities/ConnectivityCheck.java
@@ -0,0 +1,68 @@
+package com.example.jsontry02.utilities;
+
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.jsontry02.R;
+import com.example.jsontry02.activities.SplashscreenActivity;
+
+public class ConnectivityCheck extends BroadcastReceiver {
+    Context mContext;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mContext=context;
+        if (isConnected(context)){
+
+        }else{
+            showDialog();
+        }
+    }
+    public  boolean isConnected(Context context){
+        try {
+            ConnectivityManager connectivityManager=(ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo=connectivityManager.getActiveNetworkInfo();
+            return (networkInfo!=null && networkInfo.isConnected());
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void showDialog(){
+
+        Dialog dialog= new Dialog(mContext);
+        dialog.setContentView(R.layout.connection_check);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+        Button okButton= dialog.findViewById(R.id.ok);
+        Button tryButton= dialog.findViewById(R.id.tryAgain);
+        dialog.show();
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "OK", Toast.LENGTH_SHORT).show();
+                System.exit(0);
+
+            }
+        });
+        tryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "TryAgain", Toast.LENGTH_SHORT).show();
+                //need to include this feature
+            }
+        });
+
+    }
+
+}
